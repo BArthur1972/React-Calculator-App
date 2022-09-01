ecefe22c07af4e65bb2a9f71175b5732c918699a
Bethel Arthur <kbarthur14@gmail.com>
2022-09-01 12:52:22 -0500

    CSS - Style the calculators appearance using CSS.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ function App() {
 	const createDigits = () => {
 		const digits = [];
 
-		for (let i=1; i<10; i++) {
+		for (let i = 1; i < 10; i++) {
 			digits.push(
-			<button key={i}>{i}</button>
+				<button key={i}>{i}</button>
 			);
 		}
 
@@ -13,25 +13,27 @@ function App() {
 	}
 
 	return (
-		<div className="App">
-			<div className="display">
-				<span>(0)</span> 0
-			</div>
+		<div className="fi-App">
+			<div className="fi-calculator">
+				<div className="fi-display">
+					<span>(0)</span> 0
+				</div>
 
-			<div className="operators">
-				<button>/</button>
-				<button>*</button>
-				<button>+</button>
-				<button>-</button>
+				<div className="fi-operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
 
-				<button>DEL</button>
-			</div>
+					<button>DEL</button>
+				</div>
 
-			<div className="digits">
-				{ createDigits() }
-				<button>0</button>
-				<button>.</button>
-				<button>=</button>
+				<div className="fi-digits">
+					{createDigits()}
+					<button>0</button>
+					<button>.</button>
+					<button>=</button>
+				</div>
 			</div>
 		</div>
 	);
